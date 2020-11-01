f698b5ec7e53c16108d0d471d30d5ec553d33a2f
bigdaronlee163 <13237443239@163.com>
2020-11-01 15:36:39 +0800

    first commit

---
+++ src/main/java/com/whut/grpc/client/GrpcClient.java
@@ -0,0 +1,38 @@
+package com.whut.grpc.client;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.baeldung.grpc.HelloRequest;
+import org.baeldung.grpc.HelloResponse;
+import org.baeldung.grpc.HelloServiceGrpc;
+
+public class GrpcClient {
+
+public static void main(String [] args){
+
+    ManagedChannel channel = ManagedChannelBuilder
+            .forAddress("localhost",8080)
+            .usePlaintext()
+            .build() ;
+
+    HelloServiceGrpc.HelloServiceBlockingStub stub
+            = HelloServiceGrpc.newBlockingStub(channel) ;
+
+    HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+            .setFirstName("Baeldung")
+            .setLastName("gRPC")
+            .build()
+    ) ;
+
+    System.out.println(helloResponse.toString());
+
+    channel.shutdown() ;
+
+
+
+
+}
+
+
+
+}
---
+++ src/main/java/com/whut/grpc/server/HelloServiceImpl.java
@@ -0,0 +1,35 @@
+package com.whut.grpc.server;
+
+import io.grpc.stub.StreamObserver;
+import org.baeldung.grpc.HelloRequest;
+import org.baeldung.grpc.HelloResponse;
+import org.baeldung.grpc.HelloServiceGrpc;
+
+public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
+
+    @Override
+    public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver)
+    {
+
+        String greeting = new StringBuilder()
+                .append("Hello from gRPC server , ")
+                .append(request.getFirstName())
+                .append(" ")
+                .append(request.getLastName())
+                .toString();
+
+        HelloResponse response = HelloResponse.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+    }
+
+
+
+
+
+
+}
